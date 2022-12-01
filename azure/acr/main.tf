3da8e023342824b168e9c1a8f9c1468081700391
julanu <jula@Mariuss-MacBook-Pro.local>
2022-12-01 22:59:44 +0200

    refactored variables, added aks module code, updated readme

--- azure/acr/main.tf
+++ azure/acr/main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.11.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-
-  subscription_id            = var.subscription_id
-  skip_provider_registration = true
-}
-
 resource "azurerm_container_registry" "acr" {
   name                   = var.registry_name
   resource_group_name    = var.resource_group
